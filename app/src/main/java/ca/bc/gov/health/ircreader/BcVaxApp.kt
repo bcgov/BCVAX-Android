7a17eb99fbb064975eb7cebadc2ebbb373cf2237
pinakinkansara <Blackbird@2327>
2021-08-27 23:05:10 +0530

    Initial Commit

---
+++ app/src/main/java/ca/bc/gov/health/ircreader/BcVaxApp.kt
@@ -0,0 +1,8 @@
+package ca.bc.gov.health.ircreader
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BcVaxApp : Application() {
+}
---
+++ app/src/main/java/ca/bc/gov/health/ircreader/ui/scanresult/BarcodeScanResultFragment.kt
@@ -0,0 +1,20 @@
+package ca.bc.gov.health.ircreader.ui.scanresult
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ca.bc.gov.health.ircreader.R
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * [BarcodeScanResultFragment]
+ *
+ * @author Pinakin Kansara
+ */
+@AndroidEntryPoint
+class BarcodeScanResultFragment : Fragment(R.layout.fragment_barcode_scan_result) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
