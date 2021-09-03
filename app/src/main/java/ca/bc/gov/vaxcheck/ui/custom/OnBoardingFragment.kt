b54aeb2e73810cd121bcb19adab390c5d55b50da
Amit Metri <amit@freshworks.io>
2021-09-04 02:31:26 +0530

    Onboarding screen implemented

--- app/src/main/java/ca/bc/gov/vaxcheck/ui/custom/OnBoardingFragment.kt
+++ app/src/main/java/ca/bc/gov/vaxcheck/ui/custom/OnBoardingFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ca.bc.gov.vaxcheck.R
+import ca.bc.gov.vaxcheck.databinding.FragmentOnboardingBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -14,12 +16,20 @@ import ca.bc.gov.vaxcheck.R
  */
 class OnBoardingFragment : Fragment() {
 
+    private lateinit var binding: FragmentOnboardingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    ): View {
+        binding = FragmentOnboardingBinding.inflate(inflater, container, false )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.barcodeScannerFragment)
+        }
     }
 }
