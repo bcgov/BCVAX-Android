ed6439e9cd96f569106115b5f63e8981161a4be5
Amit Metri <amit@freshworks.io>
2021-09-04 02:40:12 +0530

    Used view binding extension funtion

--- app/src/main/java/ca/bc/gov/vaxcheck/ui/custom/OnBoardingFragment.kt
+++ app/src/main/java/ca/bc/gov/vaxcheck/ui/custom/OnBoardingFragment.kt
@@ -7,24 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import ca.bc.gov.vaxcheck.R
+import ca.bc.gov.vaxcheck.databinding.FragmentBarcodeScannerBinding
 import ca.bc.gov.vaxcheck.databinding.FragmentOnboardingBinding
+import ca.bc.gov.vaxcheck.utils.viewBindings
 
 /**
  * A simple [Fragment] subclass.
  * Use the [OnBoardingFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class OnBoardingFragment : Fragment() {
+class OnBoardingFragment : Fragment(R.layout.fragment_onboarding) {
 
-    private lateinit var binding: FragmentOnboardingBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentOnboardingBinding.inflate(inflater, container, false )
-        return binding.root
-    }
+    private val binding by viewBindings(FragmentOnboardingBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
