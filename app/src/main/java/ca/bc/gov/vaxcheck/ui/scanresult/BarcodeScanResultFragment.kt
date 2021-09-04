55848c8bfae6081b2530bf806a7a222501470c11
Amit Metri <amit@freshworks.io>
2021-09-04 18:36:48 +0530

    Scenes used to show different different status' of immunisation, countdown timer of 10s added in status screen.

--- app/src/main/java/ca/bc/gov/vaxcheck/ui/scanresult/BarcodeScanResultFragment.kt
+++ app/src/main/java/ca/bc/gov/vaxcheck/ui/scanresult/BarcodeScanResultFragment.kt
@@ -1,14 +1,16 @@
-package ca.bc.gov.health.ircreader.ui.scanresult
+package ca.bc.gov.vaxcheck.ui.scanresult
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.transition.Scene
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import ca.bc.gov.vaxcheck.R
 import ca.bc.gov.vaxcheck.databinding.FragmentBarcodeScanResultBinding
-import ca.bc.gov.vaxcheck.ui.scanresult.BarcodeScanResultFragmentArgs
 import ca.bc.gov.vaxcheck.utils.PayLoadProcessor
 import ca.bc.gov.vaxcheck.utils.viewBindings
 import ca.bc.gov.vaxcheck.viewmodel.BarcodeScanResultViewModel
@@ -24,17 +26,12 @@ class BarcodeScanResultFragment : Fragment(R.layout.fragment_barcode_scan_result
 
     private val binding by viewBindings(FragmentBarcodeScanResultBinding::bind)
 
-    /*
-    * Both of these variable control the UI for status screen
-    * vaccinationStatus = 0 is "No records found"
-    * vaccinationStatus = 1 is "Partially vaccinated"
-    * vaccinationStatus = 2 is "Fully vaccinated"
-    * */
-    private var userName: String = ""
-    private var vaccinationStatus: Int = 0
-
     private val args: BarcodeScanResultFragmentArgs by navArgs()
     private val viewModel: BarcodeScanResultViewModel by viewModels()
+
+    private lateinit var sceneFullyVaccinated: Scene
+    private lateinit var scenePartiallyVaccinated: Scene
+    private lateinit var sceneNoRecord: Scene
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,57 +41,52 @@ class BarcodeScanResultFragment : Fragment(R.layout.fragment_barcode_scan_result
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        sceneFullyVaccinated = Scene.getSceneForLayout(
+            binding.sceneRoot,
+            R.layout.scene_fully_vaccinated,
+            requireContext()
+        )
+        scenePartiallyVaccinated = Scene.getSceneForLayout(
+            binding.sceneRoot,
+            R.layout.scene_partially_vaccinated,
+            requireContext()
+        )
+        sceneNoRecord =
+            Scene.getSceneForLayout(binding.sceneRoot, R.layout.scene_no_record, requireContext())
+
         viewModel.observeUserName().observe(viewLifecycleOwner, {
             binding.textViewName.text = it
         })
 
-        /*
-        * Both of these variable control the UI for status screen
-        * vaccinationStatus = 0 is "No records found"
-        * vaccinationStatus = 1 is "Partially vaccinated"
-        * vaccinationStatus = 2 is "Fully vaccinated"
-        * */
         viewModel.observeVaccinationStatus().observe(viewLifecycleOwner, { immuStatus ->
             when (immuStatus!!) {
                 PayLoadProcessor.ImmuStatus.NO_RECORD -> {
-                    binding.textViewName.text = getString(R.string.no_record_found)
-                    binding.viewStatusColor.setBackgroundColor(
-                        resources.getColor(
-                            R.color.grey,
-                            null
-                        )
-                    )
-                    binding.viewLineLeft.visibility = View.INVISIBLE
-                    binding.viewLineRight.visibility = View.INVISIBLE
-                    binding.viewLineTop.visibility = View.INVISIBLE
-                    binding.viewLineBottom.visibility = View.INVISIBLE
-                    binding.textViewBCLabel.visibility = View.INVISIBLE
+                    sceneNoRecord.enter()
                 }
-                PayLoadProcessor.ImmuStatus.PARTIALLY_IMMUNIZED  -> {
-                    binding.textViewResult.text = getString(R.string.partially_vaccinated)
-                    binding.viewStatusColor.setBackgroundColor(
-                        resources.getColor(
-                            R.color.blue,
-                            null
-                        )
-                    )
+                PayLoadProcessor.ImmuStatus.PARTIALLY_IMMUNIZED -> {
+                    scenePartiallyVaccinated.enter()
                 }
-                PayLoadProcessor.ImmuStatus.FULLY_IMMUNIZED  -> {
-                    binding.imageViewRightTick.visibility = View.VISIBLE
-                    binding.textViewResult.text = getString(R.string.vaccinated)
-                    binding.viewStatusColor.setBackgroundColor(
-                        resources.getColor(
-                            R.color.green,
-                            null
-                        )
-                    )
+                PayLoadProcessor.ImmuStatus.FULLY_IMMUNIZED -> {
+                    sceneFullyVaccinated.enter()
                 }
             }
-        })
 
-        binding.imageViewClose.setOnClickListener {
-            findNavController().popBackStack()
-        }
+            /*
+            * To remove status screen after 10 seconds
+            * */
+            val countDownTimer = object : CountDownTimer(10000, 1000) {
+
+                override fun onTick(millisUntilFinished: Long) {}
+
+                override fun onFinish() {
+                    lifecycleScope.launchWhenResumed {
+                        findNavController().popBackStack()
+                    }
+                }
+            }
+            countDownTimer.start()
+
+        })
 
         binding.buttonScanAgain.setOnClickListener {
             findNavController().popBackStack()
