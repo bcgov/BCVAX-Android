6e4c47fcd71e58b7892808176330755107a6a6c0
JosephFreshworks <jose@freshworks.io>
2021-12-14 19:32:24 -0500

    yk-93 removed unused import

--- shcDecoder/src/main/java/ca/bc/gov/shcdecoder/SHCVerifierImpl.kt
+++ shcDecoder/src/main/java/ca/bc/gov/shcdecoder/SHCVerifierImpl.kt
@@ -25,7 +25,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import java.time.Instant
 import java.util.Date
 
 class SHCVerifierImpl(
